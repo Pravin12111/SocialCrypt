14f262936b3cf8d5faf04550805fc85a5d732f37
Pravin12111 <pravinchavan0656@gmail.com>
2023-10-24 16:46:20 +0530

    Edi project

---
+++ src/views/Home/Home.js
@@ -0,0 +1,14 @@
+import './Home.css';
+
+export default function Home(){
+    return(
+        <div className='Landing-page'>
+            <h1>Socialcrypt</h1>
+            <h2>Enjoy the uninterupted Music with Integrated Chat ..... </h2>
+
+            <div className='box'>
+                
+            </div>
+        </div>
+    )
+}
